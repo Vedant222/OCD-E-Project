9cf42218030d75268458ae23f138823831bfaf1c
Sanjana <sanjana@cse.iitb.ac.in>
2020-10-28 11:49:35 +0530

    second commit

--- register/views.py
+++ register/views.py
@@ -6,7 +6,16 @@ def register (response):
     if response.method == "POST":
         form = RegisterForm(response.POST)
         if form.is_valid():
-             form.save()
+             user = form.save()
+        user.refresh_from_db()
+        user.profile.first_name = form.cleaned_data.get('first_name')
+        user.profile.last_name = form.cleaned_data.get('last_name')
+        user.profile.email = form.cleaned_data.get('email')
+        user.save()
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password1')
+        user = authenticate(username=username, password=password)
+        login(request, user)
 
         return redirect("/home")    
     else:
